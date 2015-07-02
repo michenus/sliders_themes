839d3bb827f1510f919ee7b3734b63b9271df7b5
MichenusWeb <michenusweb@gmail.com>
2015-07-02 17:08:52 +0300

    add README

--- sliders/sliders-block.tpl.php
+++ sliders/sliders-block.tpl.php
@@ -13,22 +13,41 @@
  *
  * @see sliders_block_view()
  */
-
+//drupal_set_message('<pre>' . print_r($elements, 1) . '</pre>');
+$count_slider = count($elements);
 ?>
 
-<div id="slides">
-	<div class="slides_container">   
-		<?php foreach ($elements as $key => $element) {?>
-			<div>
-				<a href="<?php print $element['link']; ?>" title="Перейти" target="_blank">
-				<?php print $element['render_img']?>
-				</a>
-				<div class="caption">
-					<?php print $element['description']; ?>
-				</div>
-			</div>
-			<?php }?>          
-	</div>
-		  <a href="#" class="prev"></a>
-	  <a href="#" class="next"></a>
-</div>
+     <div id="slider">
+        <div class="main_view">
+            <div class="window">
+                <div class="image_reel">
+ 				<?php
+            	 foreach ($elements as $key => $element) {
+            	 	print '<a href="' . $element['link'] . '" title="" >' . $element['render_img'] . '</a>';  
+            	 }
+              ?>
+
+                </div>
+                <div class="descriptions">
+
+<?php
+               foreach ($elements as $key => $element) {
+                    print '<div class="desc" style="display: none;">';
+                    print $element['description'];
+                    print '</div>';
+                }
+?>
+                </div>
+            </div>
+        
+            <div class="paging">
+            <?php 
+  				for($i=0;$i<$count_slider;$i++){
+                    $s=$i+1;
+                    print '<a rel="'.$s.'" href="#">'.$s.'</a>';
+                }
+            ?>
+
+            </div>
+        </div>
+      </div><!-- EOF: #banner -->
